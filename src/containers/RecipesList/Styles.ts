77272e5fa02d262d4a8f4824c4b6ae3fc343a4c7
bucky <buckyroberts@gmail.com>
2023-05-04 19:00:15 -0400

    Recipes list styling

--- src/containers/RecipesList/Styles.ts
+++ src/containers/RecipesList/Styles.ts
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: grid;
-  gap: 24px;
-  grid-template-columns: auto 360px;
+  display: flex;
+  flex-direction: column;
+  padding: 48px;
+`;
+
+export const Img = styled.img`
+  height: auto;
+  max-width: 180px;
+  width: 100%;
+`;
+
+export const Name = styled.span`
+  font-weight: bold;
+  margin-left: 12px;
+`;
+
+export const Recipe = styled.div`
+  display: flex;
+  margin-bottom: 24px;
 `;
