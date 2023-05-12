6b59e25a48496d79f2850e0ffd67cdcf354472d2
bucky <buckyroberts@gmail.com>
2023-05-12 17:55:23 -0400

    Working on recipe details UI

--- src/pages/RecipeDetails/Styles.ts
+++ src/pages/RecipeDetails/Styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import {cardStyle} from 'styles';
+import {cardStyle, fonts} from 'styles';
 
 export const Card = styled.div`
   ${cardStyle};
@@ -11,4 +11,47 @@ export const Card = styled.div`
 export const Container = styled.div`
   margin: 0 auto;
   width: 80%;
+`;
+
+export const Details = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-top: 8px;
+`;
+
+export const Img = styled.img`
+  border-radius: 6px;
+  height: auto;
+  margin-top: 16px;
+  max-width: 100%;
+`;
+
+export const Name = styled.span`
+  font-size: 24px;
+  font-weight: ${fonts.weight.black};
+`;
+
+export const SectionContent = styled.div`
+  line-height: 1.3;
+  margin-top: 4px;
+`;
+
+export const SectionLabel = styled.div`
+  font-size: 18px;
+  font-weight: ${fonts.weight.bold};
+  margin-top: 16px;
+`;
+
+export const Top = styled.div`
+  display: flex;
+`;
+
+export const TopLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const TopRight = styled.div`
+  margin-left: 12px;
 `;
--- src/pages/RecipeDetails/index.tsx
+++ src/pages/RecipeDetails/index.tsx
@@ -1,10 +1,71 @@
+import {mdiDotsVertical} from '@mdi/js';
+
+import Detail from 'components/Detail';
+import DropdownMenu, {DropdownMenuOption} from 'components/DropdownMenu';
 import {SFC} from 'types';
 import * as S from './Styles';
 
 const RecipeDetails: SFC = ({className}) => {
+  const renderDescription = () => {
+    return (
+      <>
+        <S.SectionLabel>Description</S.SectionLabel>
+        <S.SectionContent>
+          Mushroom Risotto with Truffle Oil is a rich and savory Italian dish that's perfect for a cozy night in. This
+          dish features creamy and tender Arborio rice cooked with a flavorful combination of sautéed mushrooms, garlic,
+          onion, white wine, and parmesan cheese. To take this dish to the next level, a drizzle of aromatic truffle oil
+          is added at the end, adding a decadent and earthy flavor that perfectly complements the richness of the
+          risotto. The result is a comforting and indulgent dish that's sure to please any mushroom lover or food
+          enthusiast. Serve it as a main course or as a side dish to impress your guests at your next dinner party.
+        </S.SectionContent>
+      </>
+    );
+  };
+
+  const renderTopLeft = () => {
+    return (
+      <S.TopLeft>
+        <S.Name>Mushroom Risotto with Truffle Oil</S.Name>
+        <S.Details>
+          <Detail label="Created" value="5/9/23" />
+          <Detail label="Modified" value="5/12/23" />
+        </S.Details>
+      </S.TopLeft>
+    );
+  };
+
+  const renderTopRight = () => {
+    const menuOptions: DropdownMenuOption[] = [
+      {label: 'Edit', onClick: () => console.log(1)},
+      {label: 'Delete', onClick: () => console.log(2)},
+    ];
+
+    return (
+      <S.TopRight>
+        <DropdownMenu icon={mdiDotsVertical} options={menuOptions} />
+      </S.TopRight>
+    );
+  };
+
+  const renderTop = () => {
+    return (
+      <S.Top>
+        {renderTopLeft()}
+        {renderTopRight()}
+      </S.Top>
+    );
+  };
+
   return (
     <S.Container className={className}>
-      <S.Card>Recipe details here</S.Card>
+      <S.Card>
+        {renderTop()}
+        <S.Img
+          alt="image"
+          src="https://images.pexels.com/photos/2773940/pexels-photo-2773940.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        {renderDescription()}
+      </S.Card>
     </S.Container>
   );
 };
