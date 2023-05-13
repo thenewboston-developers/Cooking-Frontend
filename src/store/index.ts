b3b8b4fedb159f3bcc803bb871554edd5c2cfd2b
bucky <buckyroberts@gmail.com>
2023-05-13 14:43:24 -0400

    Persist store across refreshes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,33 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 
 import authenticationReducer from 'store/authentication';
 import managerReducer from 'store/manager';
 import selfReducer from 'store/self';
 
-const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     authentication: authenticationReducer,
     manager: managerReducer,
     self: selfReducer,
-  },
+  }),
+);
+
+export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE],
+      },
+    }),
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
